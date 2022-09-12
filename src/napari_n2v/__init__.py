40d0ef8da853417b50dd9e5ff8f381764e508f17
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-09-12 15:28:50 +0200

    add rgb

--- src/napari_n2v/__init__.py
+++ src/napari_n2v/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.0.1"
 
-from ._sample_data import n2v_2D_data, n2v_3D_data
+from ._sample_data import n2v_2D_data, n2v_3D_data, n2v_rgb_data
 from ._train_widget import TrainingWidgetWrapper
 from ._predict_widget import PredictWidget
--- src/napari_n2v/_sample_data.py
+++ src/napari_n2v/_sample_data.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 import os
 import urllib
 import zipfile
+from pathlib import Path
+
 import numpy as np
+from skimage import io
 
 from napari.types import LayerDataTuple
 
@@ -38,6 +41,25 @@ def _load_2D():
     return [(Train_img, {'name': 'Train'}), (Val_img, {'name': 'Val'})]
 
 
+def _load_rgb():
+    data_dir = Path('./data')
+    if not data_dir.exists():
+        os.mkdir(data_dir)
+
+    # check if data has been downloaded already
+    zipPath = Path(data_dir / 'RGB.zip')
+    if not zipPath.exists():
+        # download and unzip data
+        data = urllib.request.urlretrieve('https://download.fht.org/jug/n2v/RGB.zip', zipPath)
+        with zipfile.ZipFile(zipPath, 'r') as zip_ref:
+            zip_ref.extractall(data_dir)
+
+    img_path = Path(data_dir / 'longBeach.png')
+    img = io.imread(img_path)[..., :3]  # remove alpha channel
+
+    return [(img, {'name': 'RGB'})]
+
+
 def _n2v_data(dim):
     assert dim in [2, 3]
 
@@ -57,3 +79,7 @@ def n2v_3D_data() -> LayerDataTuple:
 
 def n2v_2D_data() -> LayerDataTuple:
     return _n2v_data(2)
+
+
+def n2v_rgb_data() -> LayerDataTuple:
+    return _load_rgb()
