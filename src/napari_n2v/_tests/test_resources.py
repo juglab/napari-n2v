af825775bd197febe128280648a0fdcf09ec06ce
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-09-20 18:06:15 +0200

    resources to str

--- src/napari_n2v/_tests/test_resources.py
+++ src/napari_n2v/_tests/test_resources.py
@@ -5,19 +5,24 @@ from napari_n2v.resources import *
 
 def test_gear():
     assert Path(ICON_GEAR).exists()
+    assert type(ICON_GEAR) == str
 
 
 def test_github():
     assert Path(ICON_GITHUB).exists()
+    assert type(ICON_GITHUB) == str
 
 
 def test_juglab():
     assert Path(ICON_JUGLAB).exists()
+    assert type(ICON_JUGLAB) == str
 
 
 def test_tf():
     assert Path(ICON_TF).exists()
+    assert type(ICON_TF) == str
 
 
 def test_bioimage():
     assert Path(DOC_BIOIMAGE).exists()
+    assert type(DOC_BIOIMAGE) == str
