9cbdf3cff4e792d1f0f08322aaf1e5a2a47293d9
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2022-09-28 17:33:17 +0200

    download data in hidden folder

--- src/napari_n2v/_sample_data.py
+++ src/napari_n2v/_sample_data.py
@@ -11,62 +11,76 @@ from skimage import io
 from napari.types import LayerDataTuple
 from napari.utils import notifications as ntf
 
+from napari_n2v.utils import cwd, get_temp_path
+
 
 def _load_3D():
     from skimage import io
 
-    zipPath = 'data/flywing-data.zip'
-    if not os.path.exists(zipPath):
-        # download and unzip data
-        urllib.request.urlretrieve('https://download.fht.org/jug/n2v/flywing-data.zip', zipPath)
-        with zipfile.ZipFile(zipPath, 'r') as zip_ref:
-            zip_ref.extractall('data')
+    with cwd(get_temp_path()):
+        data_path = Path('data')
+        if not data_path.exists():
+            data_path.mkdir()
 
-    Train_img = io.imread('data/flywing.tif')
+        # check if data has been downloaded already
+        zip_path = Path(data_path, 'flywing-data.zip')
+        if not zip_path.exists():
+            # download and unzip data
+            urllib.request.urlretrieve('https://download.fht.org/jug/n2v/flywing-data.zip', zip_path)
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(data_path)
 
-    return [(Train_img, {'name': 'Train'})]
+        im_path = Path(data_path, 'flywing.tif')
+        Train_img = io.imread(im_path)
+
+        return [(Train_img, {'name': 'Train'})]
 
 
 def _load_2D():
-    # check if data has been downloaded already
-    zipPath = "data/BSD68_reproducibility.zip"
-    if not os.path.exists(zipPath):
-        # download and unzip data
-        urllib.request.urlretrieve('https://download.fht.org/jug/n2v/BSD68_reproducibility.zip', zipPath)
-        with zipfile.ZipFile(zipPath, 'r') as zip_ref:
-            zip_ref.extractall("data")
+    with cwd(get_temp_path()):
+        data_path = Path('data')
+        if not data_path.exists():
+            data_path.mkdir()
 
-    Train_img = np.load('data/BSD68_reproducibility_data/train/DCNN400_train_gaussian25.npy')
-    Val_img = np.load('data/BSD68_reproducibility_data/val/DCNN400_validation_gaussian25.npy')
+        # check if data has been downloaded already
+        zip_path = Path(data_path, 'BSD68_reproducibility.zip')
+        if not zip_path.exists():
+            # download and unzip data
+            urllib.request.urlretrieve('https://download.fht.org/jug/n2v/BSD68_reproducibility.zip', zip_path)
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(data_path)
 
-    return [(Train_img, {'name': 'Train'}), (Val_img, {'name': 'Val'})]
+        train_path = Path(data_path, 'BSD68_reproducibility_data', 'train', 'DCNN400_train_gaussian25.npy')
+        val_path = Path(data_path, 'BSD68_reproducibility_data', 'val', 'DCNN400_validation_gaussian25.npy')
+
+        Train_img = np.load(str(train_path))
+        Val_img = np.load(str(val_path))
+
+        return [(Train_img, {'name': 'Train'}), (Val_img, {'name': 'Val'})]
 
 
 def _load_rgb():
-    data_dir = Path('./data')
-    if not data_dir.exists():
-        os.mkdir(data_dir)
+    with cwd(get_temp_path()):
+        data_dir = Path('./data')
+        if not data_dir.exists():
+            os.mkdir(data_dir)
 
-    # check if data has been downloaded already
-    zipPath = Path(data_dir / 'RGB.zip')
-    if not zipPath.exists():
-        # download and unzip data
-        urllib.request.urlretrieve('https://download.fht.org/jug/n2v/RGB.zip', zipPath)
-        with zipfile.ZipFile(zipPath, 'r') as zip_ref:
-            zip_ref.extractall(data_dir)
+        # check if data has been downloaded already
+        zipPath = Path(data_dir, 'RGB.zip')
+        if not zipPath.exists():
+            # download and unzip data
+            urllib.request.urlretrieve('https://download.fht.org/jug/n2v/RGB.zip', zipPath)
+            with zipfile.ZipFile(zipPath, 'r') as zip_ref:
+                zip_ref.extractall(data_dir)
 
-    img_path = Path(data_dir / 'longBeach.png')
-    img = io.imread(img_path)[..., :3]  # remove alpha channel
+        img_path = Path(data_dir, 'longBeach.png')
+        img = io.imread(img_path)[..., :3]  # remove alpha channel
 
-    return [(img, {'name': 'RGB'})]
+        return [(img, {'name': 'RGB'})]
 
 
 def _n2v_data(dim):
     assert dim in [2, 3]
-
-    # create a folder for our data
-    if not os.path.isdir('./data'):
-        os.mkdir('data')
 
     if dim == 2:
         return _load_2D()
